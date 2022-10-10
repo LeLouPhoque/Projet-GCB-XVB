d950aa45fa1ee86abfe870195467f3cbc506fceb
erautureau <erautureau@ST201>
2022-10-10 11:43:05 +0200

    FormDeLaM2

--- Projet-GCB-XVB/FormM2.Designer.cs
+++ Projet-GCB-XVB/FormM2.Designer.cs
@@ -29,16 +29,64 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rapport = new System.Windows.Forms.ToolStripMenuItem();
+            this.visiteur = new System.Windows.Forms.ToolStripMenuItem();
+            this.medicament = new System.Windows.Forms.ToolStripMenuItem();
+            this.medecin = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(669, 373);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Esteban Rautureau";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rapport,
+            this.visiteur,
+            this.medicament,
+            this.medecin});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // rapport
+            // 
+            this.rapport.Name = "rapport";
+            this.rapport.Size = new System.Drawing.Size(133, 20);
+            this.rapport.Text = "L’activité des visiteurs";
+            this.rapport.Click += new System.EventHandler(this.medecinToolStripMenuItem_Click);
+            // 
+            // visiteur
+            // 
+            this.visiteur.Name = "visiteur";
+            this.visiteur.Size = new System.Drawing.Size(138, 20);
+            this.visiteur.Text = "Les Visiteurs Médicaux";
+            this.visiteur.Click += new System.EventHandler(this.visiteurToolStripMenuItem_Click);
+            // 
+            // medicament
+            // 
+            this.medicament.Name = "medicament";
+            this.medicament.Size = new System.Drawing.Size(111, 20);
+            this.medicament.Text = "Les médicaments";
+            this.medicament.Click += new System.EventHandler(this.medicament_Click);
+            // 
+            // medecin
+            // 
+            this.medecin.Name = "medecin";
+            this.medecin.Size = new System.Drawing.Size(90, 20);
+            this.medecin.Text = "Les médecins";
+            this.medecin.Click += new System.EventHandler(this.medecin_Click);
             // 
             // FormM2
             // 
@@ -46,8 +94,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormM2";
             this.Text = "FormM2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +108,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rapport;
+        private System.Windows.Forms.ToolStripMenuItem visiteur;
+        private System.Windows.Forms.ToolStripMenuItem medicament;
+        private System.Windows.Forms.ToolStripMenuItem medecin;
     }
 }
--- Projet-GCB-XVB/FormM2.cs
+++ Projet-GCB-XVB/FormM2.cs
@@ -16,5 +16,34 @@ namespace Projet_GCB_XVB
         {
             InitializeComponent();
         }
+
+        private void visiteurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormM2_Visiteur form1 = new FormM2_Visiteur();
+            form1.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void medecinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormM2_Rapport form1 = new FormM2_Rapport();
+            form1.Show();
+        }
+
+        private void medicament_Click(object sender, EventArgs e)
+        {
+            FormM2_medic form1 = new FormM2_medic();
+            form1.Show();
+        }
+
+        private void medecin_Click(object sender, EventArgs e)
+        {
+            FormM2_Medecins form1 = new FormM2_Medecins();
+            form1.Show();
+        }
     }
 }
